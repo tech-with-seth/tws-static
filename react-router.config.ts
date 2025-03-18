57fbaa22f7253f29a668d49c7d148ef61351e2d0
Seth Davis <sethdavis512@gmail.com>
2025-03-18 12:10:37 -0500

    Turn ssr back on

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,7 @@ import type { Config } from '@react-router/dev/config';
 import { flatRoutes } from '@react-router/fs-routes';
 
 export default {
-    ssr: false,
+    ssr: true,
     async prerender() {
         const routes = await flatRoutes();
         return routes.map((route) => (route.path ? `/${route.path}` : '/'));
