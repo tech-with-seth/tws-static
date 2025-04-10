2485de294f11fa5dcd3fc9ef9621f98e326959e2
Seth Davis <sethdavis512@gmail.com>
2025-04-10 12:32:21 -0500

    Update config

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,7 @@ import type { Config } from '@react-router/dev/config';
 import { flatRoutes } from '@react-router/fs-routes';
 
 export default {
-    ssr: true,
+    ssr: false,
     async prerender() {
         const routes = await flatRoutes();
         return routes.map((route) => (route.path ? `/${route.path}` : '/'));
